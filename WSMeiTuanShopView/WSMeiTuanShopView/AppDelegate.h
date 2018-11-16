c702046cae29e52d629b5c61c5b2f4e40e55955c
charles <457790959@qq.com>
2018-11-16 10:58:35 +0800

    init project

---
+++ WSMeiTuanShopView/WSMeiTuanShopView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WSMeiTuanShopView
+//
+//  Created by 梧上 on 2018/11/16.
+//  Copyright © 2018 梧上. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WSMeiTuanShopView/WSMeiTuanShopView/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  WSMeiTuanShopView
+//
+//  Created by 梧上 on 2018/11/16.
+//  Copyright © 2018 梧上. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+@end
